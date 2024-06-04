0ad6fc5c12e7981d951e19e47627607dbf4a3065
nghiaphan1411 <tthanhnghia1411@gmail.com>
2024-06-04 16:15:01 +0700

    Initial Commit

---
+++ PTNetFramework/PTNetFramework.h
@@ -0,0 +1,18 @@
+//
+//  PTNetFramework.h
+//  PTNetFramework
+//
+//  Created by Nguyen Huu Tuong on 04/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PTNetFramework.
+FOUNDATION_EXPORT double PTNetFrameworkVersionNumber;
+
+//! Project version string for PTNetFramework.
+FOUNDATION_EXPORT const unsigned char PTNetFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTNetFramework/PublicHeader.h>
+
+
